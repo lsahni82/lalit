935fe1b99a74ba378c0ce55561a1eb0f064e5858
lsahni82 <lalit.sahni@gmail.com>
2017-08-26 02:51:35 +0530

    25.08.2017

---
+++ ds/factor 2.R
@@ -0,0 +1,10 @@
+#factor practice r in action
+patientid=c(1,2,3,4,5)
+age=c(25,34,28,52)
+diabtes=c("Type1", "Type2", "Type1", "Type1")
+status <- c("Poor", "Improved", "Excellent", "Poor")
+diabtes=factor(diabtes)
+summary(diabtes)
+summary(status)
+status=(factor(status,order=TRUE))
+status
